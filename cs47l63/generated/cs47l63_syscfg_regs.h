9f6b3812237fbb0d4157ba3584c13f1644fcbe3a
Kristoffer Rist Skøien <kristoffer.skoien@nordicsemi.no>
2022-03-31 16:07:44 +0200

    sdk-mcu-drivers: Added build system configs and generated files
    Added zephyr/module.yml to auto detect module.
    Generated files added as was done in an earlier version of this upstream repo
    
    Signed-off-by: Kristoffer Rist Skøien <kristoffer.skoien@nordicsemi.no>

---
+++ cs47l63/generated/cs47l63_syscfg_regs.h
@@ -0,0 +1,56 @@
+/**
+ * @file cs47l63_syscfg_regs.h
+ *
+ * @brief Register values to be applied after CS47L63 Driver boot().
+ *
+ * @copyright
+ * Copyright (c) Cirrus Logic 2022 All Rights Reserved, http://www.cirrus.com/
+ *
+ * Licensed under the Apache License, Version 2.0 (the License); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * wisce_to_syscfg_reg_converter.py SDK version: 4.11.0 - 61ee6c0881e6270b4df6e990d502275c00aad6c9
+ *
+ */
+
+#ifndef CS47L63_SYSCFG_REGS_H
+#define CS47L63_SYSCFG_REGS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/******************************************************************************
+ * INCLUDES
+ ******************************************************************************/
+#include "stdint.h"
+#include "regmap.h"
+
+/******************************************************************************
+ * LITERALS & CONSTANTS
+ ******************************************************************************/
+#define CS47L63_SYSCFG_REGS_TOTAL (30)
+
+/******************************************************************************
+ * ENUMS, STRUCTS, UNIONS, TYPEDEFS
+ ******************************************************************************/
+
+/******************************************************************************
+ * GLOBAL VARIABLES
+ ******************************************************************************/
+extern uint32_t cs47l63_syscfg_regs[];
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // CS47L63_SYSCFG_REGS_H
